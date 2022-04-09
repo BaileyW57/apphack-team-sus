e85b466576f6aa0fe89c1a89777424f4ace2acd5
BaileyW57 <williamsbs5@appstate.edu>
2022-04-08 20:47:36 -0400

    Added project

---
+++ apphack/apphack/MyClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace apphack
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+            public void main(string[] args)
+            {
+                printf("test");
+            }
+        }
+    }
+}
